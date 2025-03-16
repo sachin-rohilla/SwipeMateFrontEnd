20f598e98470ad9ce7803b668680c25b50e146db
sachin-rohilla <sachinrohilla23799@gmail.com>
2025-03-16 23:51:03 +0530

    custoom hook for get feeds added

---
+++ src/hooks/useFeeds.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { API_URL } from "../constants/constants";
+
+const useFeeds = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const getFeedsApi = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`${API_URL}/api/feeds`, {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      if (!response.ok) {
+        const errorMessage =
+          data?.message || response.statusText || "An unknown error occurred.";
+        throw new Error(` ${errorMessage}`);
+      }
+      console.log("data", data);
+    } catch (error: any) {
+      console.log("Error in getFeedsApi", error);
+      toast.error(error?.message || "Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return {
+    getFeedsApi,
+    isLoading,
+  };
+};
+export default useFeeds;
